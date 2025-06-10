4e6652d3960aee8b814135874aaa6b7a19d4a7e1
Krid Sumangsri <125956482+Kridmemo@users.noreply.github.com>
2025-06-10 12:19:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import streamlit as st
-from langchain.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
